a9d4272b7e82438d3b1b8e2a6b8db1900977a511
Zuhril Fahrizal <zuhrilfahrizal08gtg@gmail.com>
2022-05-24 14:23:10 +0700

    revisi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 $(document).ready(function () {
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
@@ -25,3 +27,52 @@ $(document).ready(function () {
         );
     });
 });
+
+var type = new Typed('.auto-tulis', {
+    strings: ['web design', 'freelancer', 'content creator', 'web development'],
+    typeSpeed: 150,
+    loop: true
+});
+
+let themeToggler = document.querySelector(".theme-toggler");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.onclick = () => {
+    themeToggler.classList.toggle("active");
+};
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+    btn.onclick = () => {
+        let color = btn.style.background;
+        document.querySelector(":root").style.setProperty("--hijau", color);
+    };
+});
+
+window.onscroll = () => {
+    themeToggler.classList.remove("active");
+};
+
+const scriptURL =
+    'https://script.google.com/macros/s/AKfycbwZPQjadLTYcWY1-KIxLSW_k8OSBFGUDPIg57e2hDZFcwqAEXW5TtxW8MClZqiEbHS1/exec';
+const form = document.forms['submit-contact'];
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            // ketika btn submit di klik tampilkan alert
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Your message has been success'
+            });
+            // reset form
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+});
+
