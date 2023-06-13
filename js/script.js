f1dc0787d88424f84403bb945b92d3c6b78acdd8
zuhrilgtg08 <zuhrilfahrizal08gtg@gmail.com>
2023-06-13 17:18:09 +0700

    commit rev

--- js/script.js
+++ js/script.js
@@ -1,73 +1,81 @@
 $(document).ready(function () {
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
-    });
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
 
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
 
-        if ($(window).scrollTop() > 0) {
-            $('.top').show();
-        } else {
-            $('.top').hide();
-        }
-    });
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
+    }
+  });
 
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-                scrollTop: $($(this).attr('href')).offset().top,
-            },
-            500,
-            'linear'
-        );
-    });
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
 });
 
-var type = new Typed('.auto-tulis', {
-    strings: ['web design', 'freelancer', 'content creator', 'web development'],
-    typeSpeed: 150,
-    loop: true
+var type = new Typed(".auto-tulis", {
+  strings: ["", "Freelancer", "Web Developer"],
+  typeSpeed: 150,
+  loop: true,
 });
 
 let themeToggler = document.querySelector(".theme-toggler");
 let toggleBtn = document.querySelector(".toggle-btn");
 
 toggleBtn.onclick = () => {
-    themeToggler.classList.toggle("active");
+  themeToggler.classList.toggle("active");
 };
 
 document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
-    btn.onclick = () => {
-        let color = btn.style.background;
-        document.querySelector(":root").style.setProperty("--hijau", color);
-    };
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--theme", color);
+    localStorage.setItem("colour", color);
+  };
+
+  if (localStorage.getItem("colour")) {
+    document
+      .querySelector(":root")
+      .style.setProperty("--theme", localStorage.getItem("colour"));
+  }
 });
 
 window.onscroll = () => {
-    themeToggler.classList.remove("active");
+  themeToggler.classList.remove("active");
 };
 
-let alert = document.querySelector('.alert');
-let cancelButton = document.querySelector('#cancel-button');
+let alert = document.querySelector(".alert");
+let cancelButton = document.querySelector("#cancel-button");
 
 const scriptURL =
-    'https://script.google.com/macros/s/AKfycbwZPQjadLTYcWY1-KIxLSW_k8OSBFGUDPIg57e2hDZFcwqAEXW5TtxW8MClZqiEbHS1/exec';
-const form = document.forms['submit-contact'];
+  "https://script.google.com/macros/s/AKfycbwZPQjadLTYcWY1-KIxLSW_k8OSBFGUDPIg57e2hDZFcwqAEXW5TtxW8MClZqiEbHS1/exec";
+const form = document.forms["submit-contact"];
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form)
-        })
-        .then(response => {
-            alert.classList.remove('hide');
-            cancelButton.addEventListener('click', () => alert.classList.add('hide'));
-            form.reset();
-            console.log('Success!', response)
-        })
-        .catch(error => console.error('Error!', error.message))
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
+    .then((response) => {
+      alert.classList.remove("hide");
+      cancelButton.addEventListener("click", () => alert.classList.add("hide"));
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
